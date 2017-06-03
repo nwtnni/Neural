65158e5ad7a369682b552756850d46812cb16ac5
Newton Ni <cn279@cornell.edu>
2017-06-02 22:38:08 -0400

    Implemented training method with backpropagation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -20,24 +20,40 @@ class NeuralNetwork:
         for i in range(0, len(self.layers) - 1):
             self.weights.append(r.rand(self.layers[i + 1], self.layers[i]) - 0.5)
 
-        print self.weights[0]
+    def train(self, examples, targets, epochs):
 
-    def train(self):
-        pass
+        for epoch in range(epochs):
+
+            # Iterate through all training examples
+            for i in range(len(examples)):
+
+                # Forward phase
+                out = [examples[i]]
+                for layer in range(len(self.layers) - 1):
+                    out.append(logistic(np.dot(self.weights[layer], out[-1])))
+
+                error = targets[i] - out[-1]
+
+                # Backward phase
+                for i in range(len(self.layers) - 1, 0, -1):
+
+                    delta = self.rate + np.dot(error * logistic(out[i]) * 1 - logistic(out[i]), out[i - 1].T)
+                    error *= self.weights[i - 1].T
+                    self.weights[i - 1] += delta
 
     def query(self, input):
 
         values = input
 
         for layer in range(len(self.layers) - 1):
-            values = np.dot(self.weights[layer], values)
-            values = [logistic(x) for x in values]
+            values = logistic(np.dot(self.weights[layer], values))
+
         return values
 
-def logistic(x):
-    return 1.0 / (1 + math.exp(-x))
+def logistic(vector):
+    return [(1.0 / (1 + math.exp(-x))) for x in vector]
 
 if __name__ == "__main__":
     r.seed(0)
-    network = NeuralNetwork([3, 2], 1)
-    print network.query(np.array([1, 1, 1]))
+    network = NeuralNetwork([3, 2, 7, 100, 200], 0.1)
+    print network.query(np.array([3, 3, 3]))
